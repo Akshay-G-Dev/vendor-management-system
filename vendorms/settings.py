4552f1b1cc4358c8df3b35d9930e454eb0388401
Akshya-G-Dev <akshaygoyal567@gmail.com>
2024-05-05 17:14:10 +0530

    Update media and static file settings

--- vendorms/settings.py
+++ vendorms/settings.py
@@ -118,8 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "static"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+MEDIA_URL = '/media/'
+STATIC_ROOT = Path(BASE_DIR / 'static')
+MEDIA_ROOT = Path(BASE_DIR / 'media')
